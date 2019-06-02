49042b83a8cc5325498073c07aa266558e9988b7
starikcetin <cetinsamedtarik@gmail.com>
2019-06-02 17:09:52 +0300

    section a finished? need to see if this is a valid thing.

---
+++ 02 - Prefab to Entity/Assets/02.A - With ConvertToEntity/Instantiator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+	
+    void Start()
+    {
+		Instantiate(_prefab, Vector3.zero, Quaternion.identity);
+    }
+}
