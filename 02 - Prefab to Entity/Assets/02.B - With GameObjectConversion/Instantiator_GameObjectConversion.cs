47a1a543e57004dfd8112b6cad12ce59e93d18b3
starikcetin <cetinsamedtarik@gmail.com>
2019-06-03 12:16:38 +0300

    02.b

---
+++ 02 - Prefab to Entity/Assets/02.B - With GameObjectConversion/Instantiator_GameObjectConversion.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class Instantiator_GameObjectConversion : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+
+    private void Start()
+    {
+        // Convert the *Prefab* to an Entity.
+        // This does not instantiate the prefab, it just registers the prefab to ECS ecosystem
+        // so we can use it later on.
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(_prefab, World.Active);
+
+        // Now instantiate another Entity out of it.
+        var entityManager = World.Active.EntityManager;
+        var entity = entityManager.Instantiate(prefab);
+    }
+}
